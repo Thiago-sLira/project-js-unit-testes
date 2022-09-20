4b05aa0e7814eb8e0332ac94219d1379d3cf28e4
Thiago Lira <thiago.slira@outlook.com>
2022-09-20 20:53:21 -0300

    Requisito 5 concluído

--- src/createStudent.js
+++ src/createStudent.js
@@ -15,6 +15,11 @@
     estudante.feedback() // Retorna: 'Eita pessoa boa!'
 */
 
-const createStudent = () => {};
+const createStudent = (nome) => ({
+      name: nome,
+      feedback: function feedback() {
+        return 'Eita pessoa boa!';
+      },
+    });
 
 module.exports = createStudent;
